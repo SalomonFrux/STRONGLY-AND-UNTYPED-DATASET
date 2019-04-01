ea7ca8e62d23010a30723a08066d7f4912618df3
SalomonFrux <47065236+SalomonFrux@users.noreply.github.com>
2019-04-01 22:50:08 +0200

    Create FORM2.StronglyTyped.cs

---
+++ FORM2.StronglyTyped.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Linq;
+
+namespace StronglyTypeDataset
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                StudentDataSetTableAdapters.StudentsTableAdapter studentsTableAdapter =
+                    new StudentDataSetTableAdapters.StudentsTableAdapter();
+                StudentDataSet.StudentsDataTable studentDatatable = new StudentDataSet.StudentsDataTable();
+                studentsTableAdapter.Fill(studentDatatable);
+
+                Session["studentDatatable"] = studentDatatable;
+                GridView1.DataSource = from Student in studentDatatable
+                                       select new
+                                       {
+                                           Student.ID,
+                                           Student.Name,
+                                           Student.Gender,
+                                           Student.TotalMarks
+                                       };
+                GridView1.DataBind();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TextBox1.Text))
+            {
+                if (Session["studentDatatable"] != null)
+
+                {
+                    StudentDataSet.StudentsDataTable studentDatatable = (StudentDataSet.StudentsDataTable)Session["studentDatatable"];
+                    GridView1.DataSource = studentDatatable;
+                    GridView1.DataBind();
+                }
+            }
+            else
+            {
+                StudentDataSet.StudentsDataTable studentDatatable = (StudentDataSet.StudentsDataTable)Session["studentDatatable"];
+                
+                GridView1.DataSource = from Student in studentDatatable
+                                       where Student.Name.ToUpper().StartsWith(TextBox1.Text)
+                                       select new
+                                       {
+                                           Student.ID,
+                                           Student.Name,
+                                           Student.Gender,
+                                           Student.TotalMarks
+                                       };
+                GridView1.DataBind();....................................must check here
+            }
+        }
+    }
+}
